07b578c2583e9432d792ea1f9f8332a7456d7282
hotsaucejake <get@hotsaucejake.com>
2020-11-14 10:17:20 -0500

    edit angular files, create view and controller, and add a web route

---
+++ resources/views/angular.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Angular</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="assets/angular/favicon.ico">
+<link rel="stylesheet" href="/assets/angular/styles.09e2c710755c8867a460.css"></head>
+<body>
+  <app-root></app-root>
+<script src="/assets/angular/runtime.8ff1d47154a726e9590c.js" defer></script><script src="/assets/angular/polyfills.bf99d438b005d57b2b31.js" defer></script><script src="/assets/angular/main.92f26f6fa5c20ea39c31.js" defer></script></body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AngularController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
